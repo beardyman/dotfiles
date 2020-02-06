e531ba0df0c2745c43dcde221677031606d28d93
Beardyman <therealbeardyman@gmail.com>
2020-02-06 13:50:20 -0500

    created basic uninstall file

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+
+
+[[ -e $HOME/.aliases ]] && rm -f $HOME/.aliases
+[[ -e $HOME/.zshenv ]] && rm -f $HOME/.zshenv
+[[ -e $HOME/.zshrc ]] && rm -f $HOME/.zshrc
+[[ -e $HOME/.utilFunctions ]] && rm -f $HOME/.utilFunctions
+[[ -e $HOME/.vimrc ]] && rm -f $HOME/.vimrc
+[[ -e $HOME/.plug.vim ]] && rm -f $HOME/.plug.vim
+
