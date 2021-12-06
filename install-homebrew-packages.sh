20ed9402c73979a78e6d63c2ba332ea727bfe7aa
Beardyman <therealbeardyman@gmail.com>
2021-12-06 00:40:54 -0500

    updated iterm2 package install

--- install-homebrew-packages.sh
+++ install-homebrew-packages.sh
@@ -2,7 +2,7 @@
 # only install if its not already installed
 [[ -z $(which brew) ]] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-[[ ! -e /Applications/iTerm-2.app ]] && brew install iTerm2
+[[ ! -e /Applications/iTerm-2.app ]] && brew install --cask iterm2
 [[ -z $(which redis-cli) ]] && brew install redis
 [[ -z $(which postgres) ]] && brew install postgres
 [[ -z $(which aws) ]] && brew install awscli
