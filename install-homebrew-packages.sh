e5eb2844d65274c30ede7fe40f7c14d6474642ad
Beardyman <therealbeardyman@gmail.com>
2021-12-06 00:50:12 -0500

    setup python install

--- install-homebrew-packages.sh
+++ install-homebrew-packages.sh
@@ -2,6 +2,15 @@
 # only install if its not already installed
 [[ -z $(which brew) ]] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# python install
+if [[ -z $(which python3) ]]; then
+  brew install python
+  
+  # sets up symlinks so that the brew installed python is the default
+  ln -s /usr/local/bin/python3 /usr/local/bin/python
+  ln -s /usr/local/bin/pip3 /usr/local/bin/pip
+fi
+
 [[ ! -e /Applications/iTerm-2.app ]] && brew install --cask iterm2
 [[ -z $(which redis-cli) ]] && brew install redis
 [[ -z $(which postgres) ]] && brew install postgres
